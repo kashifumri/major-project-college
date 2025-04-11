9ff4e7b38719e995698405f9884f7f3ac0f9e2e8
Avais Umri <avaisumar890@gmail.com>
2025-04-11 12:54:20 +0530

    major project

---
+++ party.js
@@ -0,0 +1,8 @@
+const area =document.getElementById("area-feet")
+const search_cost=document.getElementById("total-cost")
+const btn_cost=document.getElementById("btn-cost")
+function search_fun(){
+
+search_cost.innerHTML=area.value/1000*2000+20000
+}
+btn_cost=addEventListener("click",search_fun);
